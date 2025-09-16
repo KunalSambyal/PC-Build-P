93a51d01af7d040cc3f1d8c145dcc26a6421057c
Kunal Sambyal <kunalsambyal9628@gmail.com>
2025-09-16 11:53:54 +0530

    All Files

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src="/public/images/pc.svg" alt="logo" />
+        <h2>TechForge</h2>
+      </div>
+      <ul className="hide">
+        <li herf="#">Home</li>
+        <li herf="#">Products</li>
+        <li herf="#">PC Builder</li>
+        <li herf="#">Contact</li>
+      </ul>
+      <div className="search">
+        <div className="icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+        
+        <input placeholder="Search here..."/>
+      </div>
+      <div className="nav-icons">
+        <i className="fa-solid fa-magnifying-glass hide"></i>
+        <i className="fa-solid fa-cart-shopping hide"></i>
+        <i className="fa-solid fa-user hide"></i>
+        <i className="fa-solid fa-moon hide"></i>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
