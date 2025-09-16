9e25a6a03c05cd9d0c854863d36c0e443c4bef64
Kunal Sambyal <kunalsambyal9628@gmail.com>
2025-09-16 12:04:44 +0530

    New branch

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import "../App.css";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navbar from './components/navbar'
+import Navbar from './components/navbar/navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
